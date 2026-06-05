3827d262eba6e9c7451cebb1ee21033033d30404
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-06-05 18:28:26 -0300

    :truck: dbCode: Update database code schema

--- TechStockAPI/TechStockAPI/Contexts/TechStockDbContext.cs
+++ TechStockAPI/TechStockAPI/Contexts/TechStockDbContext.cs
@@ -18,18 +18,50 @@ public partial class TechStockDbContext : DbContext
 
     public virtual DbSet<Produto> Produto { get; set; }
 
+    public virtual DbSet<TipoUsuario> TipoUsuario { get; set; }
+
+    public virtual DbSet<Usuario> Usuario { get; set; }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=TechStockDb;Trusted_Connection=true;TrustServerCertificate=true");
+        => optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS; Database=TechStockDb; Trusted_Connection=true; TrustServerCertificate=true");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Produto>(entity =>
         {
-            entity.HasKey(e => e.ProdutoId).HasName("PK__Produto__9C8800E3659F9803");
+            entity.HasKey(e => e.ProdutoId).HasName("PK__Produto__9C8800E37FAAA751");
 
             entity.Property(e => e.Nome).HasMaxLength(100);
             entity.Property(e => e.Preco).HasColumnType("decimal(10, 2)");
+
+            entity.HasOne(d => d.Usuario).WithMany(p => p.Produto).HasForeignKey(d => d.UsuarioId);
+        });
+
+        modelBuilder.Entity<TipoUsuario>(entity =>
+        {
+            entity.HasKey(e => e.TipoId).HasName("PK__TipoUsua__97099EB7ADF6E62F");
+
+            entity.HasIndex(e => e.NomeTipo, "UQ__TipoUsua__7859A10AAB1EA0CE").IsUnique();
+
+            entity.Property(e => e.NomeTipo)
+                .HasMaxLength(40)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.UsuarioId).HasName("PK__Usuario__2B3DE7B811745434");
+
+            entity.HasIndex(e => e.Email, "UQ__Usuario__A9D1053433D096C6").IsUnique();
+
+            entity.Property(e => e.Email)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.NomeUsuario).HasMaxLength(100);
+            entity.Property(e => e.Senha).HasMaxLength(32);
+
+            entity.HasOne(d => d.Tipo).WithMany(p => p.Usuario).HasForeignKey(d => d.TipoId);
         });
 
         OnModelCreatingPartial(modelBuilder);

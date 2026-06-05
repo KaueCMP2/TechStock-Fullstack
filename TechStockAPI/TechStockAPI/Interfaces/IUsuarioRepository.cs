1d5b27b5c86954e5974e15e12727879d67298f19
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-06-05 18:21:39 -0300

    :plus: add: interface Usuario

---
+++ TechStockAPI/TechStockAPI/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,15 @@
+﻿using TechStockAPI.Domains;
+
+namespace TechStockAPI.Interfaces
+{
+    public interface IUsuarioRepository
+    {
+        public List<Usuario> Listar();
+        public Usuario ObterPorId(int id);
+        public Usuario ObterPorEmail(string email);
+        public void Adicionar(Usuario usuario);
+        public void Atualizar(int id, Usuario usuario);
+        public void AtualizarSenha(int id, string senha);
+        public void DeletarUsuario(int id);
+    }
+}

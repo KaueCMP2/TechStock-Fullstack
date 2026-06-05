4ed9f4b9c693cb56f8d1aba44d6b87cf4507a7ec
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-06-05 18:23:06 -0300

    :plus: add: Repository TipoUsuario

---
+++ TechStockAPI/TechStockAPI/Repositories/TipoUsuarioRepository.cs
@@ -0,0 +1,25 @@
+﻿using TechStockAPI.Contexts;
+using TechStockAPI.Domains;
+using TechStockAPI.Interfaces;
+
+namespace TechStockAPI.Repositories
+{
+    public class TipoUsuarioRepository : ITipoUsuarioRepository
+    {
+        private readonly TechStockDbContext _context;
+        public TipoUsuarioRepository(TechStockDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<TipoUsuario> Listar()
+        {
+            return _context.TipoUsuario.OrderBy(t => t.NomeTipo).ToList();
+        }
+        
+        public TipoUsuario ObterPorId(int id)
+        {
+            return _context.TipoUsuario.Find(id);
+        }
+    }
+}

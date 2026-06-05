918fd270d292a131bc55c1fe08cdb2abc396f309
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-06-05 18:22:28 -0300

    :plus: add: DTO TipoUsuario

---
+++ TechStockAPI/TechStockAPI/DTOs/TipoUsuarioDTO/LerTipoUsuarioDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TechStockAPI.DTOs.TipoUsuarioDTO
+{
+    public class LerTipoUsuarioDto
+    {
+        public int TipoId { get; set; }
+
+        public string? NomeTipo { get; set; }
+    }
+}

7f178961e47c942e2b8ce7f31da2cbe38711f6a7
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-06-05 18:32:33 -0300

    :sparkles: feat: add TipoUsuario Controller

---
+++ TechStockAPI/TechStockAPI/Controllers/TipoUsuarioController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TechStockAPI.Applications.Services;
+
+namespace TechStockAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TipoUsuarioController : ControllerBase
+    {
+        private readonly TipoUsuarioService _service;
+        public TipoUsuarioController(TipoUsuarioService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public ActionResult Listar()
+        {
+            try
+            {
+                return Ok(_service.Listar());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult ObterPorId(int id)
+        {
+            try
+            {
+                return Ok(_service.ObterPorId(id));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+    }
+}

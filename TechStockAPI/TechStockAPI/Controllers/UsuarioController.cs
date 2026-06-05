35e96567e2064ec82e516a191db31d730d277d23
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-06-05 18:32:48 -0300

    :sparkles: feat: add Usuario Controller

---
+++ TechStockAPI/TechStockAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,97 @@
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TechStockAPI.Applications.Services;
+using TechStockAPI.DTOs.UsuarioDTO;
+
+namespace TechStockAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly UsuarioService _service;
+        public UsuarioController(UsuarioService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            try
+            {
+                return Ok(_service.Listar());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult ObterPorId(int id)
+        {
+            try
+            {
+                return Ok(_service.ObterPorId(id));
+            }
+            catch (Exception ex)
+            {
+                return  BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Adicionar(CriarUsuarioDto usuarioDto)
+        {
+            try
+            {
+                return Created();
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, AtualizarUsuarioDto usuarioDto)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult AtualizarSenha(int id, string senha)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+    }
+}

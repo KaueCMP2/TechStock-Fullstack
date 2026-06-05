f62a5e3f0843f478290609928140bb66ad6dd149
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-06-05 18:21:20 -0300

    :plus: add: interface TipoUsuario

---
+++ TechStockAPI/TechStockAPI/Interfaces/ITipoUsuarioRepository.cs
@@ -0,0 +1,10 @@
+﻿using TechStockAPI.Domains;
+
+namespace TechStockAPI.Interfaces
+{
+    public interface ITipoUsuarioRepository
+    {
+        public List<TipoUsuario> Listar();
+        public TipoUsuario ObterPorId(int id);
+    }
+}

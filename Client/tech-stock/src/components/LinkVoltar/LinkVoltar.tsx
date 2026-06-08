6756d2a3d181bd7a446797600f6ef8465ee34a48
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-06-08 20:48:15 -0300

    :sparkles:feat{component]: return link partial completed

---
+++ Client/tech-stock/src/components/LinkVoltar/LinkVoltar.tsx
@@ -0,0 +1,9 @@
+import styles from "@/components/LinkVoltar/LinkVoltar.module.css"
+import Link from "next/link"
+import React from 'react'
+
+const LinkVoltar = () => {
+    return <Link href="/home">Voltar</Link>
+}
+
+export default LinkVoltar

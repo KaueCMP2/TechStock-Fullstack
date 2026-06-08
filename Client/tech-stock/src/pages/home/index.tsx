f24a61552eca8759c266a2721972dd23eecc6608
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-06-08 20:50:33 -0300

    :sparkles:feat{pages]: home page partial completed

---
+++ Client/tech-stock/src/pages/home/index.tsx
@@ -0,0 +1,16 @@
+import Banner from '@/components/Banner/Banner'
+import Header from '@/components/Header/Header'
+import ListaProdutosUsuario from '@/components/ListaProdutosUsuario/ListaProdutosUsuario'
+import React from 'react'
+
+const index = () => {
+  return (
+    <>
+      <Banner />
+      <ListaProdutosUsuario />
+    </>
+  )
+}
+
+export default index
+

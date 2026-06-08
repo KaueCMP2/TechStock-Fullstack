2653e5530296afbcac1763833926eebaf9aa50c0
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-06-08 20:46:09 -0300

    :sparkles:feat{component]: search input partial completed

---
+++ Client/tech-stock/src/components/InputPesquisa/InputPesquisa.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from '@/components/InputPesquisa/InputPesquisa.module.css' 
+
+const InputPesquisa = () => {
+  return <input id={styles.input_pesquisa} placeholder='Busque um produto'/>
+}
+
+export default InputPesquisa

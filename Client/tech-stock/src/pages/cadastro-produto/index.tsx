2686af00604085bc9ecb940140d67147b1f2da4b
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-06-09 19:52:43 -0300

    :sparkles:feat[page]: add cadastro produto

---
+++ Client/tech-stock/src/pages/cadastro-produto/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from "@/pages/cadastro-produto/cadastro.module.css"
+import FormProduto from '@/components/FormProduto/FormProduto'
+
+const index = () => {
+  return (
+    <>
+        <FormProduto />
+    </>
+  )
+}
+
+export default index
